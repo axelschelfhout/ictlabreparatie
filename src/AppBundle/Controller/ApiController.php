f938bb24b039aa1a0114a8d3ad3df8db8c957391
Axel Schelfhout <axelschelfhout@gmail.com>
2015-09-22 20:17:51 +0200

    C:/Program Files/Git/api is reachable.. that was fast

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -4,15 +4,20 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class ApiController extends Controller
 {
     /**
-     * @Route("/api")
+     * @Route("/api/{subject}")
+     * @Route("/api/{subject}/{method}")
+     * @Route("/api/{subject}/{method}/{action}")
      */
-    public function indexAction(){
-        die('Hallo wereld');
+    public function indexAction($subject = null, $method = "GET", $action = null){
+        
+        
+        
+        return new JsonResponse(array("Hallo" => "Wereld"));
     }
     
     
